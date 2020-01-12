4057c69f3af0274c05b2a46b4f32c63cd1bb42a1
Patrick Germann <30571074+patrickgermann@users.noreply.github.com>
2020-01-12 12:03:25 +0100

    Models and validation

--- db/models/course.js
+++ db/models/course.js
@@ -3,11 +3,75 @@ const Sequelize = require('sequelize');
 module.exports = (sequelize) => {
   class Courses extends Sequelize.Model {}
   Courses.init({
-    title: Sequelize.STRING,
-    description: Sequelize.TEXT,
-    estimatedTime: Sequelize.STRING,
-    materialsNeeded: Sequelize.STRING
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true, 
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: '',
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Please Provide a "Title"!'
+        },
+        notNull: {
+          args: true,
+          msg: '"Title" is Required!'
+        }
+      }
+    },
+    description: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+      defaultValue: '',
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Please provide a "Description"!'
+        },
+        notNull: {
+          args: true,
+          msg: '"Description" is Required!'
+        }
+      }
+    },
+    estimatedTime: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    materialsNeeded: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: -1,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Please provide a "User"!'
+        },
+        notNull: {
+          args: true,
+          msg: '"User" is required!'
+        }
+      }
+    }, 
   }, { sequelize });
+
+  Courses.associate = (models) => {
+    Courses.belongsTo(models.Users, {
+      as: 'owner', // Alias
+      foreignKey: {
+          fieldName: 'userId',
+          allowNull: false,
+      },
+    });
+  };
 
   return Courses;
 };
--- db/models/user.js
+++ db/models/user.js
@@ -3,10 +3,83 @@ const Sequelize = require('sequelize');
 module.exports = (sequelize) => {
     class Users extends Sequelize.Model {}
     Users.init({
-        firstName: Sequelize.STRING,
-        lastName: Sequelize.STRING,
-        emailAddress: Sequelize.STRING,
-        password: Sequelize.STRING
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        firstName: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            defaultValue: '',
+            validate: {
+              notEmpty: {
+                args: true,
+                msg: 'Please Provide a "First Name!"'
+              },
+              notNull: {
+                args: true,
+                msg: '"First Name" is Required!'
+              }
+            }
+        },
+        lastName: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            defaultValue: '',
+            validate: {
+              notEmpty: {
+                args: true,
+                msg: 'Please Provide a "Last Name!"'
+              },
+              notNull: {
+                args: true,
+                msg: '"Last Name" is Required!'
+              }
+            }
+        },
+        emailAddress: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+            defaultValue: '',
+            validate: {
+              notEmpty: {
+                args: true,
+                msg: 'Please Provide a "E-Mail"!'
+              },
+              notNull: {
+                args: true,
+                msg: '"E-Mail" is Required!'
+              }
+            }
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            defaultValue: '',
+            validate: {
+              notEmpty: {
+                args: true,
+                msg: 'Please Provide a "Password"!'
+              },
+              notNull: {
+                args: true,
+                msg: '"Password" is Required!'
+              }
+            }
+        }, 
     }, { sequelize });
+
+    Users.associate = (models) => {
+        Users.hasMany(models.Courses, {
+            as: 'owner', // Alias
+            foreignKey: {
+                fieldName: 'userId',
+                allowNull: false,
+            },
+        });
+    };
+
 return Users;
 };
